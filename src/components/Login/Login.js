102dde586c32dafb1e8b124050b340e87ef19d4c
wasiushisir <wasiuahmed410@gmail.com>
2022-04-17 22:26:30 +0600

    ading toast

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,6 +5,9 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import Googlesign from '../Googlesign/Googlesign';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Login = () => {
@@ -20,6 +23,9 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+    const [sendPasswordResetEmail, sending, error3] = useSendPasswordResetEmail(
+        auth
+      );
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -35,6 +41,14 @@ const Login = () => {
         navigate(from, { replace: true });
 
     }
+
+    const handleResetPassword= async()=>{
+        const email = emailRef.current.value;
+        await sendPasswordResetEmail(email);
+        toast('Sent email');
+
+    }
+    
     return (
 
         <div className='w-50 mx-auto'>
@@ -53,13 +67,16 @@ const Login = () => {
                     <Form.Control ref={passRef} type="password" placeholder="Password" />
                 </Form.Group>
                 <p>New in Cricket academy?<span className='ps-2'><Link className='text-decoration-none' to='/registration'> Register</Link></span></p>
-                <p className='text-danger'> {error?.message}</p>
+                <p>Forget Password<span className='ps-2'><button onClick={handleResetPassword} className='text-decoration-none btn btn-link text-primary' > Reset Password</button></span></p>
+                 <p className='text-danger'> {error?.message}</p> 
+                <p className='text-danger'> {error3?.message}</p>
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
             </Form>
 
             <Googlesign></Googlesign>
+            <ToastContainer />
         </div>
 
 
